3ecf6ae3746c25fb4954383b718732407b69a623
Ilhan Hashim <ilhanmachine@gmail.com>
2024-09-29 16:16:54 +0800

    Changed the colour to red

--- data_science.py
+++ data_science.py
@@ -11,7 +11,7 @@ ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 py_dev_y = [45372, 48876, 53850, 57287, 63016,
             65998, 70003, 70000, 71496, 75370, 83640]
 
-plt.plot(ages_x, py_dev_y, color ='b', linestyle = '-', marker = 'o', label = 'Python')
+plt.plot(ages_x, py_dev_y, color ='r', linestyle = '-', marker = 'o', label = 'Python')
 
 plt.title("Median Salary(USB) by Age")
 plt.xlabel('Ages')
