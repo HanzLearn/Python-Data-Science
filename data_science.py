49981c4ca257b994dd65e3c952767dfac3e2e4fe
Ilhan Hashim <ilhanmachine@gmail.com>
2024-09-29 16:30:49 +0800

    added soilid Lines

--- data_science.py
+++ data_science.py
@@ -5,7 +5,7 @@ ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 dev_y = [38496, 42000, 46752, 49320, 53200, 
          56000, 62316, 64928, 67317, 68748, 73752]
 
-plt.plot(ages_x, dev_y, color ='green', linestyle = '--', marker = '.', label = 'All Devs')
+plt.plot(ages_x, dev_y, color ='green', linestyle = '-', marker = '.', label = 'All Devs')
 
 ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 py_dev_y = [45372, 48876, 53850, 57287, 63016,
